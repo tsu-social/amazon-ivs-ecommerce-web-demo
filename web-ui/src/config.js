e98b41bac96dd69a29a88ae709be91960ae2dd38
Jesse Barnes <jesse.barnes@tsu.social>
2020-12-15 11:54:13 -0500

    Update config.js

--- web-ui/src/config.js
+++ web-ui/src/config.js
@@ -5,6 +5,7 @@
 export const USE_MOCK_DATA = true;
 
 // API endpoint for retrieving the products list 
+//https://a3dzlwrnma.execute-api.us-east-1.amazonaws.com/Prod/ 
 export const GET_PRODUCTS_API = "";
 
 // Default video stream to play inside the video player
