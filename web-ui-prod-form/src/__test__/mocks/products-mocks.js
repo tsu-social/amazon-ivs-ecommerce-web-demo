a4b503c217e32df7df9799bf7cef93c01da0f096
Jesse Barnes <jesse.barnes@tsu.social>
2020-12-15 12:15:14 -0500

    web-ui-form-prod
    created web-ui-form directory for dashboard ecom form src components

---
+++ web-ui-prod-form/src/__test__/mocks/products-mocks.js
@@ -0,0 +1,55 @@
+export const mockProductList = {
+  "data": {
+    "products": [
+      {
+        "id": "1000567890",
+        "name": "Eyeshadow Palette - Set of 3 Palettes",
+        "imageUrl": "images/1000567890.jpg",
+        "imageLargeUrl": "images/1000567890-lg.jpg",
+        "price": 125,
+        "discountedPrice": 99,
+        "longDescription": "Sarah uses this product to contour.  She experiences that transformation. Discovery drives innovation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate volutpat rhoncus arcu porttitor in."
+      }, {
+        "id": "1000567891",
+        "name": "Waterproof Eyeliner and Mascara",
+        "imageUrl": "images/1000567891.jpg",
+        "imageLargeUrl": "images/1000567891-lg.jpg",
+        "price": 27,
+        "discountedPrice": 27,
+        "longDescription": "Sarah advises this product by combining potent natural ingredients, ancient rituals, and modern science, ACME creates products that perform with experiences that transform. Discovery drives innovation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate volutpat rhoncus arcu porttitor in."
+      }, {
+        "id": "1000567892",
+        "name": "Divine Shine - Satin Rose Lipstick set",
+        "imageUrl": "images/1000567892.jpg",
+        "imageLargeUrl": "images/1000567892-lg.jpg",
+        "price": 35,
+        "discountedPrice": 28,
+        "longDescription": "She is combining potent natural ingredients, ancient rituals, and modern science, ACME creates products that perform with experiences that transform. Discovery drives innovation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate volutpat rhoncus arcu porttitor in."
+      }, {
+        "id": "1000567893",
+        "name": "Gloss Bomb Universal Lip Luminizer",
+        "imageUrl": "images/1000567893.jpg",
+        "imageLargeUrl": "images/1000567893-lg.jpg",
+        "price": 19,
+        "discountedPrice": 15,
+        "longDescription": "Sarah is combining potent natural ingredients, ancient rituals, and modern science, ACME creates products that perform with experiences that transform. Discovery drives innovation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate volutpat rhoncus arcu porttitor in."
+      }, {
+        "id": "1000567894",
+        "name": "7-in-1 daily wear palette essentials",
+        "imageUrl": "images/1000567894.jpg",
+        "imageLargeUrl": "images/1000567894-lg.jpg",
+        "price": 103,
+        "discountedPrice": 95,
+        "longDescription": "SHe combines potent natural ingredients, ancient rituals, and modern science, ACME creates products that perform with experiences that transform. Discovery drives innovation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate volutpat rhoncus arcu porttitor in."
+      }, {
+        "id": "1000567895",
+        "name": "EYE CARE SET - sparkle gloss eyes and lashes",
+        "imageUrl": "images/1000567895.jpg",
+        "imageLargeUrl": "images/1000567895-lg.jpg",
+        "price": 59,
+        "discountedPrice": 45,
+        "longDescription": "Sarah is combining potent natural ingredients, ancient rituals, and modern science, ACME creates products that perform with experiences that transform. Discovery drives innovation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate volutpat rhoncus arcu porttitor in."
+      }
+    ]
+  }
+}
