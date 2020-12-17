51aa2463845808a616b8cfbc46d88d64802ef0d1
Jesse Barnes <jesse.barnes@tsu.social>
2020-12-17 12:24:53 -0500

    product list + config.js
    save prod conif

--- web-ui/src/config.js
+++ web-ui/src/config.js
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT-0
 
 // Enabling USE_MOCK_DATA will not require an eCommerce demo Backend (see serverless\README.md)
-export const USE_MOCK_DATA = true;
+export const USE_MOCK_DATA = false;
 
 // API endpoint for retrieving the products list 
 //https://a3dzlwrnma.execute-api.us-east-1.amazonaws.com/Prod/ 
@@ -11,3 +11,4 @@ export const GET_PRODUCTS_API = "https://a3dzlwrnma.execute-api.us-east-1.amazon
 // Default video stream to play inside the video player
 // Replace this with your own Amazon IVS Playback URL
 export const DEFAULT_VIDEO_STREAM = "https://c800b398fe8d.us-east-1.playback.live-video.net/api/video/v1/us-east-1.968019610437.channel.2R0t7MsAsBIJ.m3u8";
+
